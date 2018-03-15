4ad029ed6b9d0a79e9c2d799f07c0a533027441f
mau5atron <gabrielbetancourt96@outlook.com>
2018-03-15 15:26:41 -0700

    Updated components, squares updated on board component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 
 // makes each squre into a buttons
 class Square extends React.Component{
+	// square component now handles state.
+	// constructor works the same way as initialize in ruby in terms of passing 
+	// in data
+	// in this case data will be passed in through 'value'
+	// removed contructor from square because it does not handle state anymore
+
 	render(){
 		return (
-			<button className="square">
+			// setting state upon square click
+			<button className="square" onClick={() => this.props.onClick()}>
 				{/* passing in value from square value in Board class into square button press */}
 				{/*props allows data(values for each square) to be passed in*/}
-
+				{/*state was props, changed to state to pass in data*/}
 				{this.props.value}
 			</button>
 		);
@@ -18,9 +25,26 @@ class Square extends React.Component{
 
 // creates all the squares for the table
 class Board extends React.Component {
+	// new constructor for squares on board
+	constructor(props){
+		super(props);
+		this.state = {
+			// array with 9 nulls
+			squares: Array(9).fill(null),
+		};
+	}
+
+	handleClick(i){
+		// call slice to copy squares array instead of changing existing array
+		// slice copies squares array prior to making changes.
+		const squares = this.state.squares.slice();
+		squares[i] = 'X';
+		this.setState({squares})
+	}
+
 	renderSquare(i){
 		// added i value to pass data through square
-		return <Square value={i}/>;
+ 		return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
 	}
 
 	render(){
