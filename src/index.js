cdd8d11650b1f72d127ee5914b3c25bab63bbb10
mau5atron <gabrielbetancourt96@outlook.com>
2018-03-17 10:48:05 -0700

    added bootstrap, changed square sizes to better suit mobile

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ class Game extends React.Component {
 			history: [{
 				squares: Array(9).fill(null),
 			}],
+			stepNumber: 0,
 			xIsNext: true,
 		};
 	}
@@ -62,7 +63,7 @@ class Game extends React.Component {
 	handleClick(i){
 		// call slice to copy squares array instead of changing existing array
 		// slice copies squares array prior to making changes.
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]){
@@ -74,13 +75,21 @@ class Game extends React.Component {
 			history: history.concat([{
 				squares: squares,
 			}]),
+			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext,
+		});
+	}
+
+	jumpTo(step){
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) === 0,
 		});
 	}
 
 	render() {
 		const history = this.state.history;
-		const current = history[history.length - 1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
 
 		const moves = history.map((step, move) => {
@@ -88,7 +97,7 @@ class Game extends React.Component {
 				'Go to move #' + move : 
 				'Go to game start';
 			return(
-				<li>
+				<li key={move}>
 					<button onClick={() => this.jumpTo(move)}>{desc}</button>
 				</li>
 			);
